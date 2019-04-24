359ce56d27300effe700f2b322476b8903f8552b
plter <xtiqin@163.com>
2019-04-24 09:44:20 +0800

    配置libuv和http-parser

---
+++ code/UnderMountain/src/macosMain/kotlin/com/plter/um/appmain/AppMain.kt
@@ -0,0 +1,8 @@
+package com.plter.um.appmain
+
+import platform.posix.printf
+
+fun main() {
+    println("Hello World 1")
+    printf("Hello \n")
+}
---
+++ code/http_parser/build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+mkdir -p out
+cd out
+cmake ..
+make
