e2e0ad849ac20b1e9652790d08af3213f86f288f
plter <xtiqin@163.com>
2019-05-10 15:37:13 +0800

    Delete old files

--- code/UnderMountain/src/macosMain/kotlin/com/plter/um/appmain/AppMain.kt
+++
@@ -1,8 +0,0 @@
-package com.plter.um.appmain
-
-import platform.posix.printf
-
-fun main() {
-    println("Hello World 1")
-    printf("Hello \n")
-}
--- code/http_parser/build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p out
-cd out
-cmake ..
-make
